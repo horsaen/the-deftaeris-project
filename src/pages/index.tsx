1aaecb1fe5d9ea1fbc01bdf7fdfc7c74bc6f19e2
horsaen <atealltheglue@protonmail.com>
2023-04-20 20:04:05 -0500

    sync

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 
 import { FaSearch } from 'react-icons/fa'
+
+// this is so dumb
+const scrollToLanguage = (e) => {
+  e.preventDefault();
+  const element = document.getElementById('languageContainer');
+  if (element) {
+    element.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth' // smooth scroll
+    })
+  }
+};
 
 function LanguageCard(props) {
   return (
@@ -25,10 +38,15 @@ export default function Home() {
       <main>
         <div className={styles.landing}>
           <div className={styles.title}>
-            <span>{`Finding language learning resources shouldn't be difficult or costly.`}</span>
+            <span>{`Finding language learning resources shouldn't be difficult or costly`}</span>
             <span>{`We compile the best resources for as many languages as we possibly can, so anyone, no matter what language they're trying to learn, can find quality resources :)`}</span>
             <div className={styles.buttonContainer}>
-
+              <a onClick={scrollToLanguage} href="#languageContainer">
+                Learn More
+              </a>
+              <Link href="sign-up">
+                <span>Sign Up</span>
+              </Link>
             </div>
           </div>
         </div>
---
+++ src/pages/sign-up/index.tsx
@@ -0,0 +1,7 @@
+export default function SignUp () {
+    return (
+        <div>
+            <span>orks</span>
+        </div>
+    )
+}
