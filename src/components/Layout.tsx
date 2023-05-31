bec3535076ef91f582d18ba9a2419b710b1287ed
horsaen <atealltheglue@protonmail.com>
2023-05-30 22:18:31 -0500

    ugh

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Layout ({ children }) {
     return (
         <>
             { children }
             {/* uncommment for prod */}
-            {/* <script async src="https://analytics.horsaen.com/script.js" data-website-id="bab3ec41-f2f4-4ed5-895b-2e63c5365c06"></script> */}
+            <Script async src={process.env.NEXT_PUBLIC_WEBURL || ""} data-website-id={process.env.NEXT_PUBLIC_WEBID || ""} />
         </>
     )
 }
